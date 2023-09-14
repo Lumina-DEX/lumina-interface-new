1f227dd7cd4afb9ee2e84ffcecad55124addd85c
gregory-lumina <gregorysantini@luminadex.com>
2023-09-14 15:13:04 -0400

    fix: build

--- ui/next.config.js
+++ ui/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  swcMinify: false,
 
   webpack(config) {
     config.resolve.alias = {
